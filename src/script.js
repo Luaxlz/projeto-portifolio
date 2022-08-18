f3bb727711f0e5c2e68d66ed3bb4c8fa7f86675c
Lucas Angeli <jl.angeli@outlook.com>
2022-08-18 13:05:13 -0300

    criando menu lateral

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    
+    // Add active class on hovered item
+    let list = document.querySelectorAll('li')
+    for (let i = 0; i < list.length; i++) {
+        list[i].mouseover = function() {
+            let j = 0
+            while (j < list.length) {
+                list[j++]
+            }
+        }
+    }
 })
